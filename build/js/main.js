aa42d29e5b0806370df5aa1f389de72f5d050f14
imbasri <imbasri.dev@gmail.com>
2024-01-29 23:54:05 +0700

    update - namescape
    untuk membungkus sebuah class / object jika ada persamaan nama dan variabel.

--- build/js/main.js
+++ build/js/main.js
@@ -1,49 +1,14 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
- * membuat interface
+ * membuat namespace
+ * dan mengambil data dengan reference path
  */
-class Employee {
-    constructor(empCode, empName) {
-        this.empCode = empCode;
-        this.empName = empName;
-    }
-    getSalary(param) {
-        if (param === this.empCode) {
-            return 1000;
-        }
-        return 0;
-    }
-}
-let emp = new Employee(1, "John");
-console.log(emp.getSalary(1));
-let person = { name: "imbasri", age: 20 };
-console.log(person);
-let hello = (name) => `hello ${name}`;
-console.log(hello("imbasri"));
-let array1 = [1, 2, 3];
-console.log(array1);
-function createSquare(config) {
-    let newSquare = { color: "white", area: 100 };
-    if (config.color) {
-        newSquare.color = config.color;
-    }
-    if (config.width) {
-        newSquare.area = config.width * config.width;
-    }
-    return newSquare;
-}
-let mySquare = createSquare({ color: "black" });
-console.log(mySquare);
-let point1 = { x: 10, y: 20 };
-console.log({ x: point1.x, y: point1.y });
-class Coba {
-    constructor(color, radius) {
-        this.color = color;
-        this.radius = radius;
-    }
-    area() {
-        return Math.PI * this.radius * this.radius;
-    }
-}
-let test = new Coba("red", 10);
-console.log(test.area());
+// / <reference path="Mahasiswa.ts" />
+const Mahasiswa_1 = require("./Mahasiswa");
+const Mahasiswa2_1 = require("./Mahasiswa2");
+let mhs1 = new Mahasiswa_1.Mydata.Mahasiswa("robot");
+let mhs2 = new Mahasiswa2_1.Mydata2.Mahasiswa("fate");
+console.log(mhs1);
+console.log(mhs2);
+// cara menjalankan reference menggunakan terminal tsc --outfile "dirOut" "dirIn"
--- src/main.ts
+++ src/main.ts
@@ -1,104 +1,14 @@
 /**
- * membuat interface
+ * membuat namespace
+ * dan mengambil data dengan reference path
  */
+// / <reference path="Mahasiswa.ts" />
+import { Mydata} from "./Mahasiswa";
+import {Mydata2} from "./Mahasiswa2";
 
-interface IEmployee {
-  empCode: number;
-  empName: string;
-  getSalary: (param: number) => number;
-}
+let mhs1 = new Mydata.Mahasiswa("robot");
+let mhs2 = new Mydata2.Mahasiswa("fate");
 
-class Employee implements IEmployee {
-  empCode: number;
-  empName: string;
-  constructor(empCode: number, empName: string) {
-    this.empCode = empCode;
-    this.empName = empName;
-  }
-  getSalary(param: number): number {
-    if (param === this.empCode) {
-      return 1000;
-    }
-    return 0;
-  }
-}
-
-let emp = new Employee(1, "John");
-console.log(emp.getSalary(1));
-
-interface Person {
-  name: string;
-  age: number;
-}
-let person:Person={ name:"imbasri",age:20 }
-console.log(person)
-
-interface Greeting{
-  (name:string):string
-}
-let hello :Greeting = (name:string) => `hello ${name}`
-console.log(hello("imbasri"))
-
-interface NumberArray {
-  [index:number]:number
-}
-
-let array1:NumberArray = [1,2,3]
-console.log(array1)
-
-interface SquareConfig{
-  color?:string
-  width?:number
-}
-function createSquare(config:SquareConfig):{color:string;area:number}{
-  let newSquare = {color:"white",area:100}
-  if(config.color){
-    newSquare.color = config.color
-  }
-  if(config.width){
-    newSquare.area = config.width*config.width
-  }
-  return newSquare
-}
-let mySquare = createSquare({color:"black"})
-console.log(mySquare)
-
-/**
- * readonly
- */
-
-interface Point{
-  readonly x:number
-  readonly y:number
-}
-
-let point1 :Point = {x:10,y:20}
-console.log({x:point1.x,y:point1.y})
-
-/**
- * extend
- */
-
-interface Shape{
-  area():number
-}
-interface Colorful{
-  color:string
-}
-
-interface Circle extends Shape,Colorful{
-  radius:number
-}
-class Coba implements Circle{
-  color:string
-  radius:number
-  constructor(color:string,radius:number){
-    this.color = color
-    this.radius = radius
-  }
-  area(): number {
-    return Math.PI * this.radius * this.radius
-  }
-}
-let test = new Coba("red",10)
-console.log(test.area())
+console.log(mhs1)
+console.log(mhs2)
+// cara menjalankan reference menggunakan terminal tsc --outfile "dirOut" "dirIn"
