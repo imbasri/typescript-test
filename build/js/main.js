81351ccc878393cfec1e313c491a953ca41f448b
imbasri <imbasri.dev@gmail.com>
2024-02-07 13:25:16 +0700

    update - access modifier
    belajar tentang menggunakan access pada sebuah object.

--- build/js/main.js
+++ build/js/main.js
@@ -1,14 +1,58 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 /**
- * membuat namespace
- * dan mengambil data dengan reference path
+ * access modifier
+ * 1.private methodd -> hanya bisa diakses class itu sendiri
+ * 2.public ->  bisa diakses di class lain
+ * 3.protected -> hanya bisa diakses di class itu sendiri dan turunannya
  */
-// / <reference path="Mahasiswa.ts" />
-const Mahasiswa_1 = require("./Mahasiswa");
-const Mahasiswa2_1 = require("./Mahasiswa2");
-let mhs1 = new Mahasiswa_1.Mydata.Mahasiswa("robot");
-let mhs2 = new Mahasiswa2_1.Mydata2.Mahasiswa("fate");
-console.log(mhs1);
-console.log(mhs2);
-// cara menjalankan reference menggunakan terminal tsc --outfile "dirOut" "dirIn"
+class Account {
+    constructor(id, name, balance) {
+        this._id = id;
+        this._name = name;
+        this._balance = balance;
+    }
+    get balance() {
+        return this._balance;
+    }
+    set balance(value) {
+        this._balance = value;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        this._id = value;
+    }
+}
+let account = new Account(1, "robot", 1000);
+console.log(account.balance);
+class Binatang {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    showInfo() {
+        console.log(`name ${this.name} age ${this.age}`);
+    }
+}
+let binatang = new Binatang("sapi", 10);
+binatang.name = "lion";
+binatang.showInfo();
+class Manusia {
+    constructor(nama) {
+        this.nama = nama;
+    }
+    setNama(nama) {
+        this.nama = nama;
+    }
+}
+let mhs = new Manusia("budi");
+console.log(mhs.nama);
+mhs.setNama("imbasri");
+console.log(mhs.nama);
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,74 @@
 /**
- * membuat namespace
- * dan mengambil data dengan reference path
+ * access modifier
+ * 1.private methodd -> hanya bisa diakses class itu sendiri
+ * 2.public ->  bisa diakses di class lain
+ * 3.protected -> hanya bisa diakses di class itu sendiri dan turunannya
  */
-// / <reference path="Mahasiswa.ts" />
-import { Mydata} from "./Mahasiswa";
-import {Mydata2} from "./Mahasiswa2";
 
-let mhs1 = new Mydata.Mahasiswa("robot");
-let mhs2 = new Mydata2.Mahasiswa("fate");
+class Account{
+  private _id: number;
+  private _name: string;
+  private _balance: number;
 
-console.log(mhs1)
-console.log(mhs2)
-// cara menjalankan reference menggunakan terminal tsc --outfile "dirOut" "dirIn"
+constructor(id: number, name: string, balance: number){
+  this._id = id;
+  this._name = name;
+  this._balance = balance;
+}
+
+  public get balance(): number {
+    return this._balance;
+  }
+  public set balance(value: number) {
+    this._balance = value;
+  }
+  public get name(): string {
+    return this._name;
+  }
+  public set name(value: string) {
+    this._name = value;
+  }
+  public get id(): number {
+    return this._id;
+  }
+  public set id(value: number) {
+    this._id = value;
+  }
+  
+}
+
+let account = new Account(1, "robot", 1000);
+console.log(account.balance);
+
+class Binatang {
+  public name: string;
+  public age:number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;    
+
+  }
+  showInfo(){
+    console.log(`name ${this.name} age ${this.age}`);
+  }
+}
+let binatang = new Binatang("sapi", 10);
+binatang.name = "lion";
+binatang.showInfo();
+
+
+class Manusia {
+  nama: string;
+  constructor(nama: string) {
+    this.nama = nama
+  }
+
+  setNama(nama:string){
+    this.nama = nama
+  }
+}
+
+let mhs = new Manusia("budi")
+console.log(mhs.nama)
+mhs.setNama("imbasri")
+console.log(mhs.nama)
