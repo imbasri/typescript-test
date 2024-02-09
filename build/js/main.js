62f0edbf697c7573d24dcd3598c142a9b6cc71d6
imbasri <imbasri.dev@gmail.com>
2024-02-10 03:28:22 +0700

    update - array and tuple
    membuat array dan tuple sesuai tipe data

--- build/js/main.js
+++ build/js/main.js
@@ -1,5 +1,34 @@
 "use strict";
 /**
- * percabangan
- * mirip semua seperti javascript dasar
+ * array and tuple
  */
+//cara mendeklarisakan array
+//1.menggunakan type[] tanda kurung
+let fruits = ["apple", "mango", "banana"];
+console.log(fruits);
+let fruits2 = ["apple", "mango", "banana"];
+console.log(fruits2);
+let fruits3;
+fruits3 = ["apple", "mango", "banana"];
+console.log(fruits3);
+let numbering;
+numbering = [1, 2, 3, 4, 5];
+console.log(numbering);
+let values = [1, "hello"];
+console.log(values);
+let testing;
+testing = ["hello", 1];
+console.log(testing);
+let fruits4 = ["grape", "mango", "banana"];
+console.log(fruits4[1]);
+//tuple -> membuat array yang berisi tipe data
+let employee = ['imbasri', 2, true];
+console.log(employee);
+let user;
+user = [1, 'imbasri', true, 'programmer'];
+console.log(user);
+let numbers;
+numbers = ['hello', 1, 2, 3, 4];
+numbers[3] = 500;
+numbers.push(20, "testing");
+console.log(numbers);
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,44 @@
 /**
- * percabangan
- * mirip semua seperti javascript dasar
+ * array and tuple 
  */
 
+//cara mendeklarisakan array
+//1.menggunakan type[] tanda kurung
+
+let fruits= ["apple", "mango", "banana"];
+console.log(fruits)
+
+let fruits2:Array<string> = ["apple", "mango", "banana"];
+console.log(fruits2)
+
+let fruits3:Array<string>;
+fruits3=["apple", "mango", "banana"];
+console.log(fruits3)
+
+let numbering :Array<number>
+numbering = [1,2,3,4,5];
+console.log(numbering)
+
+let values:Array<number|string> = [1,"hello"];
+console.log(values)
+
+let testing:(string|number)[];
+testing=["hello",1];
+console.log(testing)
+
+let fruits4 = ["grape", "mango", "banana"];
+console.log(fruits4[1])
+
+//tuple -> membuat array yang berisi tipe data
+let employee:[string,number,boolean] = ['imbasri',2, true]
+console.log(employee)
+
+let user : [number,string,boolean,string]
+user=[1,'imbasri',true,'programmer']
+console.log(user)
+
+let numbers : [first:string,second:number,...rest:number[]]
+numbers = ['hello',1,2,3,4]
+numbers[3]=500
+numbers.push(20,"testing")
+console.log(numbers)
