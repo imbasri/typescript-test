108e712eae625257736ab57c4ac8acab87ea87c7
imbasri <imbasri.dev@gmail.com>
2024-02-10 04:00:44 +0700

    update - selesai

--- build/js/main.js
+++ build/js/main.js
@@ -1,20 +1,4 @@
 "use strict";
 /**
- * union
- * penggabungan beberapa tipe data , menggunakan |
+ * tamat
  */
-let code;
-code = 123;
-code = "hello";
-// code = true ini akan error karena tipe data number dan string tidak sama
-function printCode(code) {
-    console.log(`code : ${code}`);
-    if (typeof code == "string") {
-        console.log(`string code : ${code}`);
-    }
-    else {
-        console.log(`number code : ${code}`);
-    }
-}
-printCode(123);
-printCode("hello");
--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,4 @@
 /**
- * union
- * penggabungan beberapa tipe data , menggunakan |
+ * tamat
  */
 
-let code: string | number;
-code = 123;
-code = "hello";
-// code = true ini akan error karena tipe data number dan string tidak sama
-
-function printCode(code: string | number) {
-  console.log(`code : ${code}`);
-  if (typeof code == "string") {
-    console.log(`string code : ${code}`);
-  } else {
-    console.log(`number code : ${code}`);
-  }
-}
-
-printCode(123);
-printCode("hello");
